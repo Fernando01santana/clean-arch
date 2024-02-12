834174f90c8f7becdc39fc62849798a39e72f580
Fernando01santana <cool.fernando23@gmail.com>
2024-02-12 08:50:50 -0300

    test: create test of methods getter of user

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -19,4 +19,27 @@ describe('User entities', () => {
     expect(sut.propos.password).toEqual(props.password);
     expect(sut.propos.createdAt).toBeInstanceOf(Date);
   });
+
+  it('should return Name of user', () => {
+    expect(sut.getName()).toBeDefined();
+    expect(sut.getName()).toEqual(props.name);
+    expect(typeof sut.getName()).toEqual('string');
+  });
+
+  it('should return Email of user', () => {
+    expect(sut.getEmail()).toBeDefined();
+    expect(sut.getEmail()).toEqual(props.email);
+    expect(typeof sut.getEmail()).toEqual('string');
+  });
+
+  it('should return Password of user', () => {
+    expect(sut.getPassword).toBeDefined();
+    expect(sut.getPassword()).toEqual(props.password);
+    expect(typeof sut.getPassword()).toEqual('string');
+  });
+
+  it('should return createdAt of user', () => {
+    expect(sut.getCreatedAt).toBeDefined();
+    expect(sut.getCreatedAt()).toBeInstanceOf(Date);
+  });
 });
