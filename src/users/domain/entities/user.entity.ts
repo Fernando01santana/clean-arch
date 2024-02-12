3eada8fd43052ba599798374b1d74a5426cfacff
Fernando01santana <cool.fernando23@gmail.com>
2024-02-12 08:51:07 -0300

    feat: create methods getters of data user in entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly propos: UserPropos) {
     this.propos.createdAt = this.propos.createdAt ?? new Date();
   }
+
+  getEmail(): string {
+    return this.propos.email;
+  }
+
+  getPassword(): string {
+    return this.propos.password;
+  }
+
+  getCreatedAt(): Date {
+    return this.propos.createdAt;
+  }
+
+  getName(): string {
+    return this.propos.name;
+  }
 }
