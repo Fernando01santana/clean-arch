97f36f1830d5e18a417cf4126a1c77f6b78f1812
Fernando01santana <cool.fernando23@gmail.com>
2024-02-12 08:39:55 -0300

    feat: create user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserPropos = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly propos: UserPropos) {
+    this.propos.createdAt = this.propos.createdAt ?? new Date();
+  }
+}
