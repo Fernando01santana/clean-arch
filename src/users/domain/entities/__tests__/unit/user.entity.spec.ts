f5655f01ed0d19135b3fc3aec96ce43799597cc7
Fernando01santana <cool.fernando23@gmail.com>
2024-02-15 21:50:34 -0300

    test: create test from user entity and news methods

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -16,25 +16,50 @@ describe('User entities', () => {
   });
 
   it('should return Name of user', () => {
-    expect(sut.getName()).toBeDefined();
-    expect(sut.getName()).toEqual(props.name);
-    expect(typeof sut.getName()).toEqual('string');
+    sut['name'] = 'other name';
+    expect(sut.props.name).toEqual('other name');
+    expect(typeof sut.props.name).toEqual('string');
+  });
+
+  it('should update Name of user', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toEqual('string');
   });
 
   it('should return Email of user', () => {
-    expect(sut.getEmail()).toBeDefined();
-    expect(sut.getEmail()).toEqual(props.email);
-    expect(typeof sut.getEmail()).toEqual('string');
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toEqual('string');
   });
 
   it('should return Password of user', () => {
-    expect(sut.getPassword).toBeDefined();
-    expect(sut.getPassword()).toEqual(props.password);
-    expect(typeof sut.getPassword()).toEqual('string');
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toEqual('string');
+  });
+
+  it('should update Password of user', () => {
+    sut['password'] = 'another password';
+    expect(sut.props.password).toEqual('another password');
+    expect(typeof sut.props.password).toEqual('string');
   });
 
   it('should return createdAt of user', () => {
-    expect(sut.getCreatedAt).toBeDefined();
-    expect(sut.getCreatedAt()).toBeInstanceOf(Date);
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+  it('should update Name of user using method update', () => {
+    sut.updateName('Another name');
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual('Another name');
+    expect(typeof sut.props.name).toEqual('string');
+  });
+
+  it('should update Password of user using method updatePassword', () => {
+    sut.updatePassword('Another password');
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual('Another password');
+    expect(typeof sut.props.password).toEqual('string');
   });
 });
